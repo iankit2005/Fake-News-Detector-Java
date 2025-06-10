2c41ea71464deb52e5e3db3c86d2a6408b7b56da
Ankit chaurasiya <iankitkr2005@gmail.com>
2025-06-10 20:09:04 +0530

    Update ApiCaller.java

--- Fake news detector/src/ApiCaller.java
+++ Fake news detector/src/ApiCaller.java
@@ -1,12 +1,22 @@
 public class ApiCaller {
     public static String checkHeadline(String headline) {
-        if (headline.toLowerCase().contains("earth is flat")) {
-            return "FAKE NEWS: This claim has been debunked.";
-        } else if (headline.toLowerCase().contains("covid vaccine")) {
-            return "TRUE NEWS: Verified by multiple sources.";
+
+
+        try {
+
+            if (headline.toLowerCase().contains("earth is flat")) {
+                return "FAKE NEWS: This claim has been debunked.";
+            } else if (headline.toLowerCase().contains("covid vaccine")) {
+                return "TRUE NEWS: Verified by multiple sources.";
+            }
+            else{
+                return "COULD NOT VERIFY: Not found in database.";
+            }
+        } catch (Exception e) {
+            return "Error occurred while checking headline. Please try again later.";
         }
-        else{
-            return "COULD NOT VERIFY: Not found in database.";
-        }
+
+
+
     }
 }
